8b545d699273cc99a98e736a3be24098026ca496
Sarat Tallamraju <sarattallamraju@gmail.com>
2014-11-17 07:23:00 +0000

    Flashing LED configurable

--- picircuit/components/led.py
+++ picircuit/components/led.py
@@ -1,6 +1,7 @@
 import time
 from RPi import GPIO
 from picircuit.components.component import Component
+from multiprocessing import Value
 
 
 class LED(Component):
@@ -24,12 +25,21 @@ class FlashingLED(LED):
 
     def __init__(self, pin, interval):
         super(FlashingLED, self).__init__(pin)
-        self._interval = interval
-        self._sigkill = False
+        self._interval = Value('d', interval)
 
     def _on(self):
         while True:
             super(FlashingLED, self)._on()
-            time.sleep(self._interval)
+            time.sleep(self._interval.value/2.0)
             super(FlashingLED, self)._off()
-            time.sleep(self._interval)
+            time.sleep(self._interval.value/2.0)
+
+    @property
+    def interval(self):
+        return self._interval.value
+
+    @interval.setter
+    def interval(self, interval):
+        self._interval.value = interval
+
+
